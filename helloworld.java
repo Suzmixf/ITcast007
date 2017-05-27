6ed85fd114e222a08f59304b95fbed090eb6c658
xdh <suzmixf@zoho.com.cn>
2017-05-27 11:13:03 +0800

    第一次提交

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+	public List<String> findAddMeg(){
+		
+		List<String> custType = new ArrayList<>();
+		
+		custType.add("钻石会员");
+		custType.add("黄金会员");
+		custType.add("白金会员");
+		custType.add("vip会员");
+		custType.add("普通");
+		
+		return custType;
+	}
